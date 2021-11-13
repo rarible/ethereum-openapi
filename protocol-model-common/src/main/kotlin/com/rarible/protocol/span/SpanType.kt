fb6882510b63a090d8e205c2e1f10c8b4c8dfe87
sergey.anufriev <s.anufriev@rarible.com>
2021-11-13 15:04:04 +0700

    Remove unneeded class

--- protocol-model-common/src/main/kotlin/com/rarible/protocol/span/SpanType.kt
+++
@@ -1,9 +0,0 @@
-package com.rarible.protocol.span
-
-object SpanType {
-    const val DB: String = "db"
-    const val APP: String = "app"
-    const val EXT: String = "ext"
-    const val CACHE: String = "cache"
-    const val TEMPLATE: String = "template"
-}
