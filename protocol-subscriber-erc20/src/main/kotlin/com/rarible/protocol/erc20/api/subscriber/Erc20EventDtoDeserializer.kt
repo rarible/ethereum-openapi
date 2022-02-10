e53ef10d8f00640e55508a03dcb75fb01147095f
Artem Yakischik <a.yakischik@rarible.com>
2022-02-10 11:50:53 +0400

    Instant deserialization fix for erc20 kafka

--- protocol-subscriber-erc20/src/main/kotlin/com/rarible/protocol/erc20/api/subscriber/Erc20EventDtoDeserializer.kt
+++ protocol-subscriber-erc20/src/main/kotlin/com/rarible/protocol/erc20/api/subscriber/Erc20EventDtoDeserializer.kt
@@ -1,13 +1,17 @@
 package com.rarible.protocol.erc20.api.subscriber
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import com.rarible.protocol.dto.Erc20BalanceEventDto
 import org.apache.kafka.common.serialization.Deserializer
 
 class Erc20EventDtoDeserializer : Deserializer<Erc20BalanceEventDto> {
-    private val mapper = ObjectMapper().registerKotlinModule()
+
+    private val mapper = ObjectMapper()
+        .registerKotlinModule()
+        .registerModule(JavaTimeModule())
 
     override fun deserialize(topic: String, data: ByteArray): Erc20BalanceEventDto {
         return mapper.readValue(data)
