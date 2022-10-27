e7119ebaa3c2e7ac2460506cfbb31c1d4203d9c2
Artem Yakischik <a.yakischik@rarible.com>
2022-10-27 11:27:03 +0400

    TcpConnection config removed (causes pool problems)

--- protocol-client-common/src/main/kotlin/com/rarible/protocol/client/DefaultProtocolWebClientCustomizer.kt
+++ protocol-client-common/src/main/kotlin/com/rarible/protocol/client/DefaultProtocolWebClientCustomizer.kt
@@ -26,17 +26,6 @@ class DefaultProtocolWebClientCustomizer(
 
         val client = HttpClient
             .create(provider)
-            /*.tcpConfiguration {
-                it.option(ChannelOption.SO_KEEPALIVE, true)
-                    .option(EpollChannelOption.TCP_KEEPIDLE, 300)
-                    .option(EpollChannelOption.TCP_KEEPINTVL, 60)
-                    .option(EpollChannelOption.TCP_KEEPCNT, 8)
-                    .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, DEFAULT_TIMEOUT_MILLIS.toInt())
-                    .doOnConnected { connection ->
-                        connection.addHandlerLast(ReadTimeoutHandler(DEFAULT_TIMEOUT_MILLIS, TimeUnit.MILLISECONDS))
-                        connection.addHandlerLast(WriteTimeoutHandler(DEFAULT_TIMEOUT_MILLIS, TimeUnit.MILLISECONDS))
-                    }
-            }*/
             .responseTimeout(DEFAULT_TIMEOUT)
             .followRedirect(true)
 
@@ -51,6 +40,5 @@ class DefaultProtocolWebClientCustomizer(
     companion object {
         val DEFAULT_MAX_BODY_SIZE = DataSize.ofMegabytes(10).toBytes().toInt()
         val DEFAULT_TIMEOUT: Duration = Duration.ofSeconds(30)
-        val DEFAULT_TIMEOUT_MILLIS: Long = DEFAULT_TIMEOUT.toMillis()
     }
 }
