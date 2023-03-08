85f776dd710fc10df43274a615ec763e764582cb
Artem Yakischik <a.yakischik@rarible.com>
2023-03-08 12:27:17 +0500

    feat: Gateway and unlockable removed

--- protocol-subscriber-nft-order/src/main/kotlin/com/rarible/protocol/nftorder/api/subscriber/NftOrderEventsConsumerFactory.kt
+++ protocol-subscriber-nft-order/src/main/kotlin/com/rarible/protocol/nftorder/api/subscriber/NftOrderEventsConsumerFactory.kt
@@ -5,11 +5,7 @@ import com.rarible.core.kafka.json.JsonDeserializer
 import com.rarible.ethereum.domain.Blockchain
 import com.rarible.protocol.dto.ActivityDto
 import com.rarible.protocol.dto.ActivityTopicProvider
-import com.rarible.protocol.dto.NftOrderItemEventDto
-import com.rarible.protocol.dto.NftOrderItemEventTopicProvider
-import com.rarible.protocol.dto.NftOrderOwnershipEventDto
-import com.rarible.protocol.dto.NftOrderOwnershipEventTopicProvider
-import java.util.*
+import java.util.UUID
 
 class NftOrderEventsConsumerFactory(
     private val brokerReplicaSet: String,
@@ -26,34 +22,6 @@ class NftOrderEventsConsumerFactory(
             valueClass = ActivityDto::class.java,
             consumerGroup = consumerGroup,
             defaultTopic = ActivityTopicProvider.getTopic(environment, blockchain.value),
-            bootstrapServers = brokerReplicaSet
-        )
-    }
-
-    fun createNftOrderItemConsumer(
-        consumerGroup: String,
-        blockchain: Blockchain
-    ): RaribleKafkaConsumer<NftOrderItemEventDto> {
-        return RaribleKafkaConsumer(
-            clientId = "${createClientIdPrefix(blockchain)}.nft-order-item-consumer",
-            valueDeserializerClass = JsonDeserializer::class.java,
-            valueClass = NftOrderItemEventDto::class.java,
-            consumerGroup = consumerGroup,
-            defaultTopic = NftOrderItemEventTopicProvider.getTopic(environment, blockchain.value),
-            bootstrapServers = brokerReplicaSet
-        )
-    }
-
-    fun createNftOrderOwnershipConsumer(
-        consumerGroup: String,
-        blockchain: Blockchain
-    ): RaribleKafkaConsumer<NftOrderOwnershipEventDto> {
-        return RaribleKafkaConsumer(
-            clientId = "${createClientIdPrefix(blockchain)}.nft-order-ownership-consumer",
-            valueDeserializerClass = JsonDeserializer::class.java,
-            valueClass = NftOrderOwnershipEventDto::class.java,
-            consumerGroup = consumerGroup,
-            defaultTopic = NftOrderOwnershipEventTopicProvider.getTopic(environment, blockchain.value),
             bootstrapServers = brokerReplicaSet
         )
     }
