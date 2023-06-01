91ff6a691820d3482d812866f1f61521c0981dbf
Artem Yakischik <a.yakischik@rarible.com>
2023-06-01 16:51:40 +0400

    feat: Common EventTimeMarks

--- protocol-model-common/src/main/kotlin/com/rarible/protocol/dto/eventTimeMarkExt.kt
+++ protocol-model-common/src/main/kotlin/com/rarible/protocol/dto/eventTimeMarkExt.kt
@@ -1,30 +1,8 @@
 package com.rarible.protocol.dto
 
-import java.time.Instant
+import com.rarible.core.common.EventTimeMark
+import com.rarible.core.common.EventTimeMarks
 
-fun blockchainEventMark(markName: String, seconds: Long): EventTimeMarksDto {
-    return blockchainEventMark(markName, Instant.ofEpochSecond(seconds))
-}
-
-fun blockchainEventMark(markName: String, date: Instant): EventTimeMarksDto {
-    return EventTimeMarksDto("blockchain")
-        .add("source", date)
-        .add(markName, Instant.now())
-}
-
-fun integrationEventMark(markName: String, date: Instant): EventTimeMarksDto {
-    return EventTimeMarksDto("integration")
-        .add("source", date)
-        .add(markName, Instant.now())
-}
-
-fun offchainEventMark(markName: String): EventTimeMarksDto {
-    return EventTimeMarksDto("offchain").add(markName, null)
-}
-
-fun EventTimeMarksDto.add(name: String, date: Instant? = null): EventTimeMarksDto {
-    val mark = EventTimeMarkDto(name, date ?: Instant.now())
-    val marks = this.marks.toMutableList()
-    marks.add(mark)
-    return this.copy(marks = marks)
+fun EventTimeMarksDto.toModel(): EventTimeMarks {
+    return EventTimeMarks(this.source, this.marks.map { EventTimeMark(it.name, it.date) })
 }
